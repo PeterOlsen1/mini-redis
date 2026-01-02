601fc629a31c5b7dfda826f7bb8da85b7a9fe3a5
PeterOlsen1 <peterolsen455@gmail.com>
2026-01-01 23:52:42 -0600

    test: update tests

--- test/client_test.go
+++ test/client_test.go
@@ -12,6 +12,94 @@ func TestInit(t *testing.T) {
 	}
 
 	defer c.Close()
+}
 
-	c.Ping()
+func TestPing(t *testing.T) {
+	c, err := client.NewClient(&client.Options{Addr: "localhost:6379"})
+	if err != nil {
+		t.Errorf("client initialization")
+	}
+
+	defer c.Close()
+
+	s, err := c.Ping("")
+	if err != nil {
+		t.Errorf("ping command (%s)", err)
+	}
+	if s != "PONG" {
+		t.Errorf("ping not met with PONG (%s)", s)
+	}
+
+	s, err = c.Ping("HELLO")
+	if err != nil {
+		t.Errorf("ping command (%s)", err)
+	}
+	if s != "HELLO" {
+		t.Errorf("ping not met with HELLO (%s)", s)
+	}
+}
+
+func TestEcho(t *testing.T) {
+	c, err := client.NewClient(&client.Options{Addr: "localhost:6379"})
+	if err != nil {
+		t.Errorf("client initialization")
+	}
+
+	defer c.Close()
+
+	s, err := c.Echo("HELLO")
+	if err != nil {
+		t.Errorf("ping command (%s)", err)
+	}
+	if s != "HELLO" {
+		t.Errorf("ping not met with HELLO (%s)", s)
+	}
+}
+
+func TestSet(t *testing.T) {
+	c, err := client.NewClient(&client.Options{Addr: "localhost:6379"})
+	if err != nil {
+		t.Errorf("client initialization")
+	}
+
+	defer c.Close()
+
+	s, err := c.Set("test", "test")
+	if err != nil {
+		t.Errorf("set command (%s)", err)
+	}
+	if s != "OK" {
+		t.Errorf("set not met with OK (%s)", s)
+	}
+}
+
+func TestGet(t *testing.T) {
+	c, err := client.NewClient(&client.Options{Addr: "localhost:6379"})
+	if err != nil {
+		t.Errorf("client initialization")
+	}
+
+	defer c.Close()
+
+	s, err := c.Get("test")
+	if err != nil {
+		t.Errorf("get command (%s)", err)
+	}
+	if s != "" {
+		t.Errorf("get not met with \"\" (%s)", s)
+	}
+
+	// set test key
+	c.Set("test", "TEST")
+	if err != nil {
+		t.Errorf("set command (%s)", err)
+	}
+
+	s, err = c.Get("test")
+	if err != nil {
+		t.Errorf("get command (%s)", err)
+	}
+	if s != "TEST" {
+		t.Errorf("get not met with TEST (%s)", s)
+	}
 }
