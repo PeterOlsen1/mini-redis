5683c987baaa4e4169e65fcc7970e5e566a79f47
Peter Olsen <olse0321@umn.edu>
2026-01-13 07:22:08 -0800

    refactor: simplify server start procedure

---
+++ test/main_test.go
@@ -0,0 +1,22 @@
+package miniredis_test
+
+// improve upon this later. not working for now
+
+// func TestMain(m *testing.M) {
+// 	// Redirect server logs to /dev/null
+// 	nullFile, _ := os.Open(os.DevNull)
+// 	defer nullFile.Close()
+
+// 	// Replace the default logger's output
+// 	log.SetOutput(nullFile)
+
+// 	// Start the server
+// 	start.Start("~/.mini-redis/test-config.yaml")
+// 	defer start.Stop()
+
+// 	// Run the tests
+// 	code := m.Run()
+
+// 	// Exit with the test result code
+// 	os.Exit(code)
+// }
